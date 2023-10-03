3d4f60b639303d952d4cc70561a8114a35f94444
lalBi94 <bilal@boudjemline.fr>
2023-10-03 23:15:31 +0200

    $

---
+++ Tests/ZNodes.spec.js
@@ -0,0 +1,28 @@
+import ZNodes from "../ZStorage/ZNodes.js"
+
+describe("ZNodes", () => {
+    it("Checking equality", () => {
+        let n = new ZNodes(5)
+        let n2 = new ZNodes(6)
+
+        expect(n.equals(n2))
+            .toBe(false)
+    })
+
+    it("Breaking and Getting next nodes, Setting a new node, ", () => {
+        let n = new ZNodes("fes")
+        let n2 = new ZNodes(5)
+        let n3 = new ZNodes(5.5)
+
+        n.setNext(n2)
+        n2.setNext(n3)
+
+        expect(n.getNext().getNext().getValue())
+            .toBe(5.5)
+
+        n2.breakNext()
+
+        expect(n.getNext().getNext())
+            .toBe(null)
+    })
+})
